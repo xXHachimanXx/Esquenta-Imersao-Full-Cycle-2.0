00a40f09a1e03cade4d5216b865eef70ff4ae2bd
xXHachimanXx <globaldep90@gmail.com>
2021-04-03 20:49:24 -0300

    Adding lost files.

--- iniciando-react/src/App.tsx
+++ iniciando-react/src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FormEvent, useRef } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Form } from "./components/Form";
+import { Title } from "./components/Title";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title onClick={() => alert('Primeiro')}>Iniciando com React</Title>
+      <Title onClick={() => alert('Segundo')} color="green">Iniciando com React</Title>
+      <Form />
     </div>
   );
 }
 
 export default App;
+
+//.ts - TypeScript - JSX
+//.js - JavaScript
+
+//.jsx
+
+//f(x) => html
+
+//Angular - Framework, arquitetura de modules, injecao de dependencia, formularios, camada http
+//React - Biblioteca
