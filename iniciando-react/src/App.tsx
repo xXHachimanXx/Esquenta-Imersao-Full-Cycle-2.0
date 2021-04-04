55b26b80b6d012086fcb69dd43f3054474d52ed9
xXHachimanXx <globaldep90@gmail.com>
2021-04-04 14:24:31 -0300

    Adding some lost files.

--- iniciando-react/src/App.tsx
+++ iniciando-react/src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FormEvent, useRef } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Form } from "./components/Form";
+import { Title } from "./components/Title";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title onClick={() => alert('Primeiro')}>Iniciando com React</Title>
+      <Title onClick={() => alert('Segundo')} color="green">Iniciando com React</Title>
+      <Form />
     </div>
   );
 }
